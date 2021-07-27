acc735f337c2e3ab8010ee103a971733ec140c4f
Juan <juantvaldez85.@gmail.com>
2021-07-27 21:00:39 +0000

    addition functions using return

--- conditionals.js
+++ conditionals.js
@@ -1,19 +1,34 @@
-const raining = true;
-const cold = false;
+// const raining = true;
+// const cold = false;
 
-if (raining) {
-  console.log("Don't forget your umbrella!");
-}
+// if (raining) {
+//   console.log("Don't forget your umbrella!");
+// }
 
-if (cold) {
-  console.log("Make sure you pick out a scarf!");
-}
+// if (cold) {
+//   console.log("Make sure you pick out a scarf!");
+// }
 
-console.log("Now you'reready to go out!");
+// console.log("Now you'reready to go out!");
 
-const isCitizen = true;
-const age = 26;
+// const isCitizen = true;
+// const age = 26;
 
-if (isCitizen && age > 18) {
-  console.log("You are eligible to vote.");
-}
+// if (isCitizen && age > 18) {
+//   console.log("You are eligible to vote.");
+// }
+
+const whichSchool = function (age) {
+  // Your code in hereif(age  < 13){
+  if (age >= 13 && age <= 18) {
+    console.log("Secondary School");
+  } else if (age >= 19 && age <= 100) {
+    console.log("Lighthouse labs");
+  } else if (age < 13) {
+    console.log("Elementary school");
+  }
+};
+
+whichSchool(12);
+whichSchool(15);
+whichSchool(100);
--- hello.js
+++ hello.js
@@ -4,10 +4,35 @@
 
 // sayHello();
 
-const sayHello = function (name) {
-  console.log("Hello, " + name);
+// const sayHello = function (name) {
+//   console.log("Hello, " + name);
+// };
+
+// sayHello("Caliban");
+// sayHello("Miranda");
+// sayHello("Ferdinand");
+
+const returnSayHello = function (name) {
+  return "Hello, " + name;
+};
+const greeting = returnSayHello("John");
+console.log(greeting);
+
+// const isEven = function (num) {
+//   return num % 2 === 0;
+// };
+
+// const tenIsEven = isEven(10);
+// const elevenIsEven = isEven(11);
+
+// console.log(tenIsEven);
+// console.log(elevenIsEven);
+
+// Simplified version of above commented "isEven"
+
+const isEven = function (num) {
+  return num % 2 === 0;
 };
 
-sayHello("Caliban");
-sayHello("Miranda");
-sayHello("Ferdinand");
+console.log(isEven(10));
+console.log(isEven(11));
