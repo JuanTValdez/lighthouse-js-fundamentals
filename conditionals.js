c0a2b0acae56c4b8e411f28ce32e45e2b4bf31a3
Juan <juantvaldez85.@gmail.com>
2021-07-28 05:16:55 +0000

    added evaluations.js to test prep evaluations

--- conditionals.js
+++ conditionals.js
@@ -17,18 +17,3 @@
 // if (isCitizen && age > 18) {
 //   console.log("You are eligible to vote.");
 // }
-
-const whichSchool = function (age) {
-  // Your code in hereif(age  < 13){
-  if (age >= 13 && age <= 18) {
-    console.log("Secondary School");
-  } else if (age >= 19 && age <= 100) {
-    console.log("Lighthouse labs");
-  } else if (age < 13) {
-    console.log("Elementary school");
-  }
-};
-
-whichSchool(12);
-whichSchool(15);
-whichSchool(100);
